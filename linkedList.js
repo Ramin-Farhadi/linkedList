1ed2a9e228325e67aff04667bc3a20394b83131b
Ramin <farhadiramin91@gmail.com>
2022-11-30 01:19:54 -0500

    Linked list + append + prepend

--- linkedList.js
+++ linkedList.js
@@ -1,1 +1,41 @@
 // creating a linked list
+
+// 5 --> 16 --> 3
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+  }
+
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+  }
+}
+
+const myLinkedList = new LinkedList(3);
+
+myLinkedList.append(44);
+myLinkedList.append(325);
+// myLinkedList.prepend(1);
+console.log(myLinkedList);
