89a63fa5c2eaa5f1f95d7b01e20a21789591d442
Ramin <farhadiramin91@gmail.com>
2022-12-03 23:15:17 -0500

    Linked list reverse method was added

--- linkedList.js
+++ linkedList.js
@@ -31,18 +31,56 @@ class LinkedList {
     this.head = newNode;
     this.length++;
   }
-  insert(index, value) {}
+  insert(index, value) {
+    const newNode = {
+      value,
+      next: null,
+    };
+    if (index < 1) {
+      return this.prepend(value);
+    }
+    let lead = this.head;
+    let holdpointer = lead.next;
+    for (let i = 0; i < index - 1; i++) {
+      lead = lead.next;
+      holdpointer = holdpointer.next;
+    }
+    lead.next = newNode;
+    newNode.next = holdpointer;
+    this.length++;
+  }
+
+  reverse() {
+    let prevNode = null;
+    let currNode = this.head;
+    let nextNode = currNode.next;
+
+    this.tail = currNode;
+    while (currNode) {
+      nextNode = currNode.next;
+      currNode.next = prevNode;
+      prevNode = currNode;
+      currNode = nextNode;
+    }
+    this.head = prevNode;
+  }
+  printOut() {
+    let next = this.head;
+    const printArray = [];
+    while (next) {
+      printArray.push(next.value);
+      next = next.next;
+    }
+    return printArray;
+  }
 }
 
 const myLinkedList = new LinkedList(3);
 
 myLinkedList.append(44);
 myLinkedList.append(325);
+myLinkedList.append(25);
 // myLinkedList.prepend(1);
-console.log(myLinkedList);
 
-let node = myLinkedList.head.next;
-for (let i = 0; i < myLinkedList.length; i++) {
-  node.add('.next');
-  console.log(node);
-}
+myLinkedList.reverse();
+console.log(myLinkedList.printOut());
